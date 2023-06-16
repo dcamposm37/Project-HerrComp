804a87ba48af55ea09d817c5293b018e6a384781
Diego Alejandro Campos <dcamposm@unal.edu.co>
2023-06-16 15:37:54 -0500

    Update mean distance and generate velocites
    Now mean distance take in account the z component and take the CG as default point. The function generate_velocities now return a tuples list with the velocities components per bodie

--- project/functions.py
+++ project/functions.py
@@ -6,20 +6,21 @@ def random_coordinates(mean, sigma):
   z = 0.05 * np.random.normal(mean, sigma) 
   return (x, y, z)
 
-# Retorna distancia promedio entre un punto y los demas puntos
-def mean_distance(points, current_point): 
+# Retorna distancia promedio entre un punto y los demas puntos. El punto por defecto es el centro de gravedad (0,0,0) (aproximado)
+def mean_distance(points, current_point = (0, 0, 0)):
   total_distance = 0
   x = current_point[0]
   y = current_point[1]
+  z = current_point[2]
 
   for point in points:
-    distance = math.sqrt((point[0] - x)**2 + (point[1] - y)**2)
+    distance = math.sqrt((point[0] - x)**2 + (point[1] - y)**2 + (point[2] - z)**2)
     total_distance += distance
 
   current_mean_distance = total_distance/len(points)
   return current_mean_distance
 
-# Retorna la distancia promedio entre todos los puntos
+# Retorna la distancia promedio entre todos los puntos. SE USA SOLO EN LA PRUEBA PARA CALCULAR LA DISTANCIA PROMEDIO ENTRE LOS CUERPOS
 def total_mean_distance(points):
   total_mean_distance = 0
   for ii in points:
@@ -27,25 +28,27 @@ def total_mean_distance(points):
   
   return total_mean_distance/len(points)
 
-
+# Retorna una lista de tuplas con las coordenadas iniciales de los 1000 cuerpos
 def random_pos(num_puntos):
     points = []
-    points.append((0, 0, 0))  # Agregamos el primer punto en el origen
-    
+    points.append(random_coordinates(0, 400))  # Agregamos el primer punto en el origen
+
     while len(points) < num_puntos:
-        # Si la distancia promedio cumple con el criterio, agregamos el punto a la lista
-        if len(points) <= 650:
+        # Agrega primero 650 puntos en torno al centro
+        if len(points) < 650:
           punto = random_coordinates(0, 400)
-          current_mean_distance = mean_distance(points, punto)
+          current_mean_distance = mean_distance(points, punto) # SE USA SOLO EN LA PRUEBA PARA CALCULAR LA DISTANCIA PROMEDIO ENTRE LOS CUERPOS
           points.append(punto)
+
+        # Los demas puntos orbitan alrededor del centro
         else:
           punto = random_coordinates(0, 1000)
-          current_mean_distance = mean_distance(points, punto)
+          current_mean_distance = mean_distance(points, punto) # SE USA SOLO EN LA PRUEBA PARA CALCULAR LA DISTANCIA PROMEDIO ENTRE LOS CUERPOS
           points.append(punto)
-    
+
     return points
 
-# Ejemplo de uso
+# PRUEBA DE QUE SI FUNCIONA
 # num_puntos= 1000
 
 # pos_ini = random_pos(num_puntos)
@@ -80,9 +83,8 @@ cdf = MB_CDF(vs,mass,T) # essentially y = f(x)
 
 #create interpolation function to CDF
 inv_cdf = interp(cdf,vs) # essentially what we have done is made x = g(y) from y = f(x)
-                         # this can now be used as a function which is 
-                         # called in the same way as normal routines
 
+# Return a tuples arrangement with the 3 velocities components per bodie, where |v| = 1 and vz aproxx 0
 def generate_velocities(n):
     """ generate a set of velocity vectors in 3D from the MB inverse CDF function """
     rand_nums = np.random.random(n)
@@ -95,22 +97,34 @@ def generate_velocities(n):
     # convert to cartesian units
     vx = speeds * np.sin(theta) * np.cos(phi) 
     vy = speeds * np.sin(theta) * np.sin(phi)
-    vz = speeds * np.cos(theta) * 0.1
+    vz = speeds * np.cos(theta) * 0.1         # z component 10 times lower 
 
-    return vx, vy, vz
+    return list(zip(vx, vy, vz))
      
 
-spd, vx, vy, vz = generate_velocities(1000)
+# PRUEBA DE QUE SI FUNCIONA
+# spd, vel_ini = generate_velocities(100000)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+# vx = [p[0] for p in vel_ini]
+# vy = [p[1] for p in vel_ini]
+# vz = [p[2] for p in vel_ini]
 
-# generate histogram of velocities
-ax.hist(spd,bins=50,fc='b',alpha=0.4,lw=0.2)
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
 
-# verify that the average velocity norm is close to zero
-speed = 0
-for i in range(1000):
-  speed += np.sqrt(vx[i]**2+vy[i]**2+vz[i]**2)
+# # generate histogram of velocities
+# ax.hist(spd,bins=50,fc='b',alpha=0.4,lw=0.2)
 
-print(speed/1000)
+# # verify that the average velocity norm is close to zero
+# speed = 0
+# for i in range(100000):
+#   speed += np.sqrt(vx[i]**2+vy[i]**2+vz[i]**2)
+
+# print(speed/100000)
+
+############################################ PROPERTIES FOR THE 3 FIGURE
+# mean distance to the center gravity
+# def mean_distance_CG():
+
+# mean velocity to the center gravity
+# def mean_vel_CG():
