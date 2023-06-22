30c911fcdaf26a88c5d1e743bd4206ca5e6e10d4
JuanSierra2003 <jsierraj@unal.edu.co>
2023-06-22 15:40:56 -0500

    gitignore

--- project/main.py
+++ project/main.py
@@ -5,14 +5,14 @@ from functions import generate_velocities, random_pos
 
 G, H = 1, 5*10**(-8)
 
-N_bodies = 100
-N_steps = 10
-step_size = 50
-dt = .01
+N_bodies = 500
+N_steps = 1000
+step_size = 10
+dt = .1
 
 data = {}
-n_bodies = np.zeros(N_steps)
-time = np.zeros(N_steps)
+n_bodies = np.zeros(N_steps+1)
+time = np.zeros(N_steps+1)
 
 np.random.seed(6642)
 
@@ -21,15 +21,21 @@ velocities = generate_velocities(N_bodies)
 bodies = [body(1, positions[ii], velocities[ii]) for ii in range(N_bodies)]
 
 u1 = universe(bodies, G, H, dt)
+u1.put(body(5, (-1500, -3000, 0), (0, 70, 0)))
+u1.put(body(5, (1500, 3000, 0), (0, -70, 0)))
+
 print('created')
 
-for ii in range(N_steps*step_size):
+for ii in range(N_steps*step_size+1):
 
 	if not ii%step_size:
 		u1.data().to_csv('data/frame%.d.csv'%(ii/step_size))
 		n_bodies[int(ii/step_size)] = u1.size
 		time[int(ii/step_size)] = u1.time
-		print("%05.2f"%((ii+1)/(step_size*N_steps)*100) + '%' + ': %.d'%(u1.size))
+		print("%05.2f"%((ii+1)/(step_size*N_steps)*100) + '%' + ': %.d'%(u1.size)+ ' %.d'%(ii/step_size))
+		if not int(ii/step_size+1)%10:
+			u1.put(body(5, (-1500, -3000, 0), (0, 70, 0)))
+			u1.put(body(5, (1500, 3000, 0), (0, -70, 0)))
 		
 	u1.evolve()
 
